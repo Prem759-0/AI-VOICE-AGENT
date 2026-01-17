6563043e28a520ca29587d9714ce19c3612a0661
premagikwad1213 <a70064182@gmail.com>
2026-01-17 23:43:02 +0530

    new update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
      <div>
       <h2>sdfg</h2>
+      <Button>hdfhgd</Button>
       </div>
   );
 }
